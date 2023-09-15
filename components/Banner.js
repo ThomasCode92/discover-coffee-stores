10ac84fd347cf87270450901dd8cff2d2579102d
ThomasCode92 <thomascode92@hotmail.com>
2023-09-15 07:00:16 +0200

    Introduction

---
+++ components/Banner.js
@@ -0,0 +1,15 @@
+import styles from '@/styles/Banner.module.css';
+
+export default function Banner({ buttonText, onClick }) {
+  return (
+    <header className={styles.container}>
+      <h1 className={styles.title}>
+        Coffee <span>Connoisseur</span>
+      </h1>
+      <p className={styles['sub-title']}>Discover your local coffee shops!</p>
+      <button className={styles.button} onClick={onClick}>
+        {buttonText}
+      </button>
+    </header>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
+import Head from 'next/head';
+
+import Banner from '@/components/Banner';
+import styles from '@/styles/Home.module.css';
+
 export default function Home() {
-  return <h1>Coffee Connoisseur</h1>;
+  const handleClick = () => {
+    console.log('Button Clicked!');
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Coffee Connoisseur</title>
+        <meta name="description" content="Find the best coffee shops" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <Banner buttonText={'View stores nearby'} onClick={handleClick} />
+      </main>
+    </div>
+  );
 }
