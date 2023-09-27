880d1b8796cdf5ea15ede699bdfd14ede352c84e
ThomasCode92 <thomascode92@hotmail.com>
2023-09-27 21:40:26 +0200

    Styling the Homepage

--- components/Banner.js
+++ components/Banner.js
@@ -7,9 +7,11 @@ export default function Banner({ buttonText, onClick }) {
         Coffee <span>Connoisseur</span>
       </h1>
       <p className={styles['sub-title']}>Discover your local coffee shops!</p>
-      <button className={styles.button} onClick={onClick}>
-        {buttonText}
-      </button>
+      <div className={styles['button-wrapper']}>
+        <button className={styles.button} onClick={onClick}>
+          {buttonText}
+        </button>
+      </div>
     </header>
   );
 }
