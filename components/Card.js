c8482cec85a5966b13bd080f3fb74ba9d6ffb416
ThomasCode92 <thomascode92@hotmail.com>
2023-10-09 09:59:26 +0200

    Card Component Styling

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,22 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import cls from 'classnames';
+
+import styles from './Card.module.css';
 
 export default function Card({ name, imageUrl, href }) {
   return (
     <Link href={href}>
-      <h2>{name}</h2>
-      <Image src={imageUrl} alt="coffee store image" width={260} height={160} />
+      <div className={cls('glass', styles.container)}>
+        <h2 className={styles['card-header']}>{name}</h2>
+        <Image
+          src={imageUrl}
+          alt="coffee store image"
+          className={styles['card-image']}
+          width={260}
+          height={160}
+        />
+      </div>
     </Link>
   );
 }
