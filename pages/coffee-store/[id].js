0eed0bc53925e664765a3fe4dbd4faa5e2016fc8
ThomasCode92 <thomascode92@hotmail.com>
2023-11-06 18:23:59 +0100

    Implement getStaticProps and getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import coffeeStoresData from '@/data/coffee-stores.json';
+
 export default function CoffeeStore() {
   const { query } = useRouter();
 
@@ -10,4 +12,16 @@ export default function CoffeeStore() {
       <Link href="/">Back to Home</Link>
     </div>
   );
+}
+
+export function getStaticProps({ params }) {
+  const coffeeStore = coffeeStoresData.find(
+    coffeeStore => coffeeStore.id === params.id
+  );
+
+  return { props: { coffeeStore } };
+}
+
+export function getStaticPaths() {
+  return { paths: [{ params: { id: '0' } }, { params: { id: '1' } }] };
 }
