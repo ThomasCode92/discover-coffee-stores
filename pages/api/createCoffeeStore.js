4a5eef3ea372bff34e42ed1fb822dd705e24873a
ThomasCode92 <thomascode92@hotmail.com>
2023-11-30 18:45:19 +0100

    Airtable Library

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,9 @@
+import Airtable from 'airtable';
+
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_API_KEY,
+});
+
+const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
+const table = base('coffee-stores');
