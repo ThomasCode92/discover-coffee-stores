d1e77b438d93f2686e88b89faa48974a041ce804
ThomasCode92 <thomascode92@hotmail.com>
2023-12-04 18:21:10 +0100

    Transform Coffee Store Data

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -9,20 +9,25 @@ const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
 const table = base('coffee-stores');
 
 export default async function createCoffeeStore(req, res) {
-  if (req.method === 'POST') {
-    const existingCoffeeStore = await table
-      .select({ filterByFormula: `id="0"` })
-      .firstPage();
+  try {
+    if (req.method === 'POST') {
+      const existingCoffeeStore = await table
+        .select({ filterByFormula: `id="0"` })
+        .firstPage();
 
-    if (existingCoffeeStore && existingCoffeeStore.length > 0) {
-      return res.status(200).json({
-        message: 'Coffee store already exists',
-        data: existingCoffeeStore,
-      });
+      if (existingCoffeeStore && existingCoffeeStore.length > 0) {
+        return res.status(200).json({
+          message: 'Coffee store already exists',
+          data: existingCoffeeStore[0].fields,
+        });
+      }
+
+      return res.json({ message: 'Creating a coffee store' });
     }
 
-    return res.json({ message: 'Creating a coffee store' });
+    return res.json({ message: 'Hello World' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Something went wrong', error });
   }
-
-  return res.json({ message: 'Hello World' });
 }
