454d2daf88582feb18422f5e11e19fb00fbcdef7
ThomasCode92 <thomascode92@hotmail.com>
2023-12-13 20:21:24 +0100

    Coffee Store By Id API Validation

--- pages/api/getCoffeeStoreById.js
+++ pages/api/getCoffeeStoreById.js
@@ -1,3 +1,5 @@
+import { table, getMinifiedRecords } from '@/lib/airtable';
+
 export default async function getCoffeeStoreById(req, res) {
   const { id } = req.query;
 
@@ -6,7 +8,18 @@ export default async function getCoffeeStoreById(req, res) {
   }
 
   try {
-    res.status(200).json({ message: 'Found coffee store' });
+    const existingCoffeeStores = await table
+      .select({ filterByFormula: `id="${id}"` })
+      .firstPage();
+
+    if (!existingCoffeeStores || existingCoffeeStores.length === 0) {
+      return res.status(404).json({ message: 'Coffee Store not found' });
+    }
+
+    return res.status(200).json({
+      message: 'Found coffee store',
+      data: getMinifiedRecords(existingCoffeeStores),
+    });
   } catch (error) {
     console.error('An unexpected error has occurred');
     console.error(error);
